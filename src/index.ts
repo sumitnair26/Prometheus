adbea8172fddeb18d0fd510ef901c5df54f05762
sumitnair26 <sumit.nair26@gmail.com>
2024-05-29 13:34:20 +0530

    added middleware to log time took for API to complete operation

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
 import express from "express";
 const app = express();
 
+//Midleware to track time taken by API
+
+function middleware(req, res, next) {
+    const startTime = Date.now();
+    next();
+    const endTime = Date.now();
+    console.log('it took', endTime - startTime, 'ms');
+}
+app.use(middleware);
+
 app.get("/user", (req, res)=>{
+    let user = {
+        name: "Sumit",
+        age:20
+    }
+    //some expensive DB operation come here 
     res.json({
         name:"Sumit"
     })
@@ -12,3 +27,5 @@ app.post("/user", (req, res) =>{
         name:"Sumit"
     })
 })
+
+app.listen(3000);
